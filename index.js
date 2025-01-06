1ee704b095c79ad67e2455cc070444e66b326f30
Jewel Galido <jewel.b.galido12@gmail.com>
2025-01-07 00:32:51 +0800

    Adding npm module from inquirer and qr-image

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import inquirer from "inquirer";
-var qr = require('qr-image');
+import qr from "qr-image" ;
+import fs from "fs";
 
+ // var qr=require("qr-image");  remove since we already assigned our code as module type
 inquirer
   .prompt([
-   {
-    message: "Type in your URL: ",
-    name: "URL",
-   },
-   
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const url = answers.URL;
+
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+
   })
   .catch((error) => {
     if (error.isTtyError) {
